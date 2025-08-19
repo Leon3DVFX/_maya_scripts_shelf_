7df64a63118d610f28a4f34b00822723c704e697
Leonid <leonid.Borolis@icloud.com>
2025-08-19 15:45:15 +0600

    Update full_screen.py

--- full_screen.py
+++ full_screen.py
@@ -1,3 +1,6 @@
+#full_screen.py
+#Author: Leon3DVFX
+#License: MIT
 from maya import OpenMayaUI as omui
 from PySide6 import QtWidgets,QtCore
 from shiboken6 import wrapInstance
@@ -9,4 +12,5 @@ def get_maya_main_window():
 mw = get_maya_main_window()
 flag0 = mw.windowState()
 flag1 = QtCore.Qt.WindowState.WindowFullScreen
+
 mw.setWindowState(flag1 ^ flag0)
