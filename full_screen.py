f986fb674cfd5b188bd8eabd1d00bb3497caa969
Leonid <leonid.Borolis@icloud.com>
2025-08-19 15:06:49 +0600

    Add files via upload

---
+++ full_screen.py
@@ -0,0 +1,12 @@
+from maya import OpenMayaUI as omui
+from PySide6 import QtWidgets,QtCore
+from shiboken6 import wrapInstance
+
+def get_maya_main_window():
+    ptr = omui.MQtUtil.mainWindow()  
+    return wrapInstance(int(ptr), QtWidgets.QWidget)
+
+mw = get_maya_main_window()
+flag0 = mw.windowState()
+flag1 = QtCore.Qt.WindowState.WindowFullScreen
+mw.setWindowState(flag1 ^ flag0)
